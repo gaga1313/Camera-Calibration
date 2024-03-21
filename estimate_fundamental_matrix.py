ff924c4c33b4803aa2c0fe5ded0654d4c8fd3b3a
Gaurav Gaonkar <63440697+gaga1313@users.noreply.github.com>
2024-03-21 01:43:57 -0400

    estimate_fundamental_matrix.py

--- estimate_fundamental_matrix.py
+++ estimate_fundamental_matrix.py
@@ -3,53 +3,6 @@ import cv2
 import random
 import matplotlib.pyplot as plt
 
-def calculate_projection_matrix(image, markers):
-    
-    dictionary = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_1000)
-    parameters = cv2.aruco.DetectorParameters_create()
-
-    markerCorners, markerIds, rejectedCandidates = cv2.aruco.detectMarkers(
-        image, dictionary, parameters=parameters)
-    markerIds = [m[0] for m in markerIds]
-    markerCorners = [m[0] for m in markerCorners]
-
-    points2d = []
-    points3d = []
-
-    for markerId, marker in zip(markerIds, markerCorners):
-        if markerId in markers:
-            for j, corner in enumerate(marker):
-                points2d.append(corner)
-                points3d.append(markers[markerId][j])
-
-    points2d = np.array(points2d)
-    points3d = np.array(points3d)
-
-    n = points2d.shape[0]
-    data_matrix = np.zeros((2 * n, 11))
-    coefficient_vector = np.zeros((2 * n, 1))
-
-    for i in range(0, n):
-        X = points3d[i, 0]
-        Y = points3d[i, 1]
-        Z = points3d[i, 2]
-        u = points2d[i, 0]
-        v = points2d[i, 1]
-
-        data_matrix[(2 *
-                     i), :] = [X, Y, Z, 1, 0, 0, 0, 0, -u * X, -u * Y, -u * Z]
-        data_matrix[(2 * i) +
-                    1, :] = [0, 0, 0, 0, X, Y, Z, 1, -v * X, -v * Y, -v * Z]
-        coefficient_vector[(2 * i)] = u
-        coefficient_vector[(2 * i) + 1] = v
-
-    M, residual, rank, s = np.linalg.lstsq(data_matrix, coefficient_vector, rcond=None)
-    M = np.append(M, 1)
-    M = np.reshape(M, (3, 4))
-
-    return M, residual
-
-
 def normalize_coordinates(Points):
    
     n = Points.shape[0]
@@ -190,25 +143,6 @@ def ransac_fundamental_matrix(matches1, matches2, num_iters):
 
     return best_Fmatrix, best_inliers1, best_inliers2, best_inlier_residual
 
-
-def visualize_ransac():
-    iterations = np.arange(len(inlier_counts))
-    best_inlier_counts = np.maximum.accumulate(inlier_counts)
-    best_inlier_residuals = np.minimum.accumulate(inlier_residuals)
-
-    plt.figure(1, figsize = (8, 8))
-    plt.subplot(211)
-    plt.plot(iterations, inlier_counts, label='Current Inlier Count', color='red')
-    plt.plot(iterations, best_inlier_counts, label='Best Inlier Count', color='blue')
-    plt.xlabel("Iteration")
-    plt.ylabel("Number of Inliers")
-    plt.title('Current Inliers vs. Best Inliers per Iteration')
-    plt.legend()
-
-    plt.subplot(212)
-    plt.plot(iterations, inlier_residuals, label='Current Inlier Residual', color='red')
-    plt.plot(iterations, best_inlier_residuals, label='Best Inlier Residual', color='blue')
-    plt.xlabel("Iteration")
     plt.ylabel("Residual")
     plt.title('Current Residual vs. Best Residual per Iteration')
     plt.legend()
