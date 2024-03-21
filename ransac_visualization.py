f1635e49d78816fdcfc016dcb17fe0ebeafb04eb
Gaurav Gaonkar <63440697+gaga1313@users.noreply.github.com>
2024-03-21 01:44:45 -0400

    ransac_visualization.py

---
+++ ransac_visualization.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def visualize_ransac():
+    iterations = np.arange(len(inlier_counts))
+    best_inlier_counts = np.maximum.accumulate(inlier_counts)
+    best_inlier_residuals = np.minimum.accumulate(inlier_residuals)
+
+    plt.figure(1, figsize = (8, 8))
+    plt.subplot(211)
+    plt.plot(iterations, inlier_counts, label='Current Inlier Count', color='red')
+    plt.plot(iterations, best_inlier_counts, label='Best Inlier Count', color='blue')
+    plt.xlabel("Iteration")
+    plt.ylabel("Number of Inliers")
+    plt.title('Current Inliers vs. Best Inliers per Iteration')
+    plt.legend()
+
+    plt.subplot(212)
+    plt.plot(iterations, inlier_residuals, label='Current Inlier Residual', color='red')
+    plt.plot(iterations, best_inlier_residuals, label='Best Inlier Residual', color='blue')
+    plt.xlabel("Iteration")
